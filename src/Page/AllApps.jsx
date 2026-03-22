6e18691c61b4cb13ecf0d48cf045acbfc093a966
hasanujjamanjibon <hasanujjamanjibon2@gmail.com>
2026-03-22 20:25:42 +0600

    All Apps page loading problem solved

--- src/Page/AllApps.jsx
+++ src/Page/AllApps.jsx
@@ -76,7 +76,7 @@ const AllApps = () => {
       </div>
       <div className='container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6 place-items-center'>
         {loading || searchLoading
-          ? Array(8)
+          ? Array(12)
               .fill(0)
               .map((_, i) => <SkeletonCard key={i} />)
           : filteredApps &&
@@ -89,7 +89,9 @@ const AllApps = () => {
               />
             ))}
       </div>
-      {!loading && filteredApps.length === 0 && <AppNotFoundPage />}
+      {!loading && !searchLoading && filteredApps.length === 0 && (
+        <AppNotFoundPage />
+      )}
     </div>
   );
 };
